f016bf73aae56001c61fa7c6b0e8e1702cbc6372
Alessandro Rodrigues de Carvalho <alessandroas2023@gmail.com>
2026-09-09 20:31:53 -0300

    Corrige importacao da criptografia de senha

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
+from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
