899ad4953c1e558cd2f129f180e3722799e1eed7
Alessandro Rodrigues de Carvalho <alessandroas2023@gmail.com>
2026-09-09 23:19:06 -0300

    Finaliza sistema de login com menu inteligente

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 import os, datetime
 import sqlite3
@@ -12,6 +13,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.context_processor
+def inject_user():
+    return dict(current_user=current_user)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 with app.app_context():
     db.create_all()
